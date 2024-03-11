079078dec812080f04e0556c11cf74c14f59769d
Nimetullah <nimetullahcezlan@gmail.com>
2024-03-11 23:42:57 +0300

    new

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,10 +1,21 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return "hello word"
 
+@app.route('/second')
+def second():
+    return "This is second page"
 
+@app.route('/third')
+def third():
+    return "flask-01-02/This is third page"
 
-
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
 
 if __name__ == '__main__':
 
